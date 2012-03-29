eeaa358e065f14695aca3acf034048bd679b8a7e
destinydriven <kurn.lamontagne@gmail.com>
2012-03-29 17:18:32 -0400

    Initial commit

---
+++ Model/FancyboxAppModel.php
@@ -0,0 +1,16 @@
+<?php
+/*
+ * Model/FancyboxAppModel.php
+ * CakePHP Fancybox Plugin
+ *
+ * Copyright (c) 2012 destinydriven (Kurn La Montagne)
+ * 
+ * Author email: d3stinydriv3n@gmail.com
+ *
+ * Licensed under MIT
+ * http://www.opensource.org/licenses/mit-license.php
+ */
+ 
+class FancyboxAppModel extends AppModel {
+}
+?>
